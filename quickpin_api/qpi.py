29c1c3177940dfdf482f382f53a3b09834d1bb74
Luke Maxwell <lukemaxwellshouse@gmail.com>
2015-12-08 16:58:09 +0000

    Change user/pass env variables to token, add skeleton for search.

--- quickpin_api/qpi.py
+++ quickpin_api/qpi.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """
 Wrapper for the QuickPin API.
 
 Includes a simple command line client.
 
-Example:
-    $ python qpi.py submit_names usernames.csv twitter --interval=5
+Example: $ python qpi.py submit_names usernames.csv twitter --interval=5
 
     This will parse the usernames contained (1 per line) in the usernames.csv
     file and submit them 1 by one at an interval of 5 seconds.
@@ -17,15 +15,13 @@ Example:
         $ python qpi.py submit_ids --help
 
     Set the  environment variables to avoid being prompted each time:
-        1. QUICKPIN_USER
-        2. QUICKPIN_PASSWORD
-        3. QUICKPIN_URL
+        1. QUICKPIN_URL
+        1. QUICKPIN_TOKEN
 
 
     Example:
-        $ export QUICKPIN_USER=guest
-        $ export QUICKPIN_PASSWORD=password
-        $ export QUICKPIN_URL=https://example.com
+        $ export QUICKPIN_URL="https://example.com"
+        $ export QUICKPIN_TOKEN="1|2015-12-09T16:50:59.057635.Y5pm9qB_naw6FkOekcksiFRyMlY"
 """
 
 import requests
@@ -33,38 +29,59 @@ import click
 import os
 import time
 import sys
+from pprint import pprint
 
 
 class QPIError(Exception):
-   """
+    """
     Represents a human-facing exception.
-   """
-   def __init__(self, message):
-       self.message = message
+    """
+    def __init__(self, message):
+        self.message = message
 
 
 class QPI():
 
-    def __init__(self, app_url, username, password, disable_warnings=True):
+    def __init__(self,
+                 app_url,
+                 token=None,
+                 username=None,
+                 password=None,
+                 disable_warnings=True):
+
         if disable_warnings:
             requests.packages.urllib3.disable_warnings()
+
         self.app_url = app_url.rstrip('/')
         self.username = username
         self.password = password
         self.auth_url = app_url + '/api/authentication/'
         self.profile_url = app_url + '/api/profile/'
+        self.search_url = app_url + '/api/search/'
         self.headers = {}
-        self.authenticated = False
+        self.token = token
 
-    def authenticate(self):
+        if self.token is None or self.token == '':
+            self.token = self.get_token(username, password)
+
+        self.headers['X-Auth'] = self.token
+        self.authenticated = True
+
+    def get_token(self, username, password):
         """
         Obtain an API token with supplied credentials.
+        If token is passed as a parameter, sets the auth header and
+        authenticated status.
         """
-        payload = {'email': self.username, 'password': self.password}
+        payload = {'email': username, 'password': password}
         response = requests.post(self.auth_url, json=payload, verify=False)
         response.raise_for_status()
-        self.headers['X-Auth'] = response.json()['token']
-        self.authenticated = True
+        try:
+            token = response.json()['token']
+        except KeyError:
+            raise QPIError('Authentication failed.')
+
+        return token
 
     def submit_user_ids(self,
                         user_ids,
@@ -135,9 +152,9 @@ class QPI():
             }
             profiles.append(profile)
         responses = self.submit_profiles(profiles=profiles,
-                                        stub=stub,
-                                        chunk_size=chunk_size,
-                                        interval=interval)
+                                         stub=stub,
+                                         chunk_size=chunk_size,
+                                         interval=interval)
         return responses
 
     def submit_profiles(self, profiles, stub=False, chunk_size=1, interval=5):
@@ -180,14 +197,56 @@ class QPI():
                     'stub': stub
                 }
                 response = requests.post(self.profile_url,
-                                        headers=self.headers,
-                                        json=payload,
-                                        verify=False)
+                                         headers=self.headers,
+                                         json=payload,
+                                         verify=False)
                 response.raise_for_status()
                 responses.append(response.content)
                 time.sleep(interval)
 
             return responses
+
+    def search(self, query, type_=None, facets=None, rpp=100,
+               page=1, sort=None):
+        """
+        Obtain QuickPin search results for query.
+
+        Args:
+            query (str): the search query.
+            type_ (str): the type, e.g. profile, post.
+            rpp (int): results per page
+            page (int): results page index.
+            sort (str): facet to search by.
+
+        Example:
+            >> qpi = QPI()
+            >> qpi.search('', '', '')
+
+        """
+        if not self.authenticated:
+            raise QPIError("Please authenticate first.")
+
+        params = {
+            'query': query,
+            'rpp': rpp,
+            'page': page,
+        }
+
+        if type_ is not None:
+            params['type'] = type_
+        if facets is not None:
+            params['facets'] = facets
+        if sort is not None:
+            params['sort'] = sort
+
+        response = requests.get(self.profile_url,
+                                headers=self.headers,
+                                params=params,
+                                verify=False)
+        print(response.request.url)
+        response.raise_for_status()
+
+        return response
 
 
 @click.group()
@@ -207,22 +266,17 @@ def cli():
 @click.option('--interval',
               default=5,
               help='request interval in seconds')
-@click.option('--username',
+@click.option('--token',
               prompt=True,
-              default=lambda: os.environ.get('QUICKPIN_USER', ''))
-@click.option('--password',
-              prompt=True,
-              hide_input=True,
-              default=lambda: os.environ.get('QUICKPIN_PASSWORD', ''))
+              default=lambda: os.environ.get('QUICKPIN_TOKEN', ''))
 @click.option('--url',
               prompt=True,
               default=lambda: os.environ.get('QUICKPIN_URL', ''))
 @click.argument('input', type=click.File('rb'))
 @click.argument('site', type=click.Choice(['twitter', 'instagram']))
-def submit_names(input, site, stub, chunk, interval, username, password, url):
+def submit_names(input, site, stub, chunk, interval, token, url):
     usernames = []
-    qpi = QPI(url, username, password)
-    qpi.authenticate()
+    qpi = QPI(token=token)
     usernames = input.read().splitlines()
     if len(usernames) == 0:
         click.echo('Empty file')
@@ -247,21 +301,17 @@ def submit_names(input, site, stub, chunk, interval, username, password, url):
 @click.option('--interval',
               default=5,
               help='request interval in seconds')
-@click.option('--username',
+@click.option('--token',
               prompt=True,
-              default=lambda: os.environ.get('QUICKPIN_USER', ''))
-@click.option('--password',
-              prompt=True,
-              hide_input=True,
-              default=lambda: os.environ.get('QUICKPIN_PASSWORD', ''))
+              default=lambda: os.environ.get('QUICKPIN_TOKEN', ''))
 @click.option('--url',
               prompt=True,
               default=lambda: os.environ.get('QUICKPIN_URL', ''))
 @click.argument('input', type=click.File('rb'))
 @click.argument('site', type=click.Choice(['twitter', 'instagram']))
-def submit_ids(input, site, stub, chunk, interval, username, password, url):
+def submit_ids(input, site, stub, chunk, interval, token, url):
     user_ids = []
-    qpi = QPI(url, username, password)
+    qpi = QPI(token=token)
     qpi.authenticate()
     user_ids = input.read().splitlines()
     if len(user_ids) == 0:
@@ -273,6 +323,63 @@ def submit_ids(input, site, stub, chunk, interval, username, password, url):
                                     chunk_size=chunk,
                                     interval=interval)
     click.echo(responses)
+
+
+@cli.command()
+@click.option('--type',
+              type=click.STRING,
+              help='the type, e.g. profile, stub')
+@click.option('--facets',
+              type=click.STRING,
+              help='facet filters')
+@click.option('--page',
+              default=1,
+              type=click.INT,
+              help='result page index')
+@click.option('--rpp',
+              default=100,
+              type=click.INT,
+              help='results per page')
+@click.option('--sort',
+              type=click.STRING,
+              help='column to sort by')
+@click.option('--token',
+              prompt=True,
+              default=lambda: os.environ.get('QUICKPIN_TOKEN', ''))
+@click.option('--url',
+              prompt=True,
+              default=lambda: os.environ.get('QUICKPIN_URL', ''))
+@click.argument('query', type=click.STRING)
+def search(query, type, facets, page, rpp, sort, token, url):
+    if token is None:
+        raise QPIError('No token found. Please authenticate first by running '
+                       'qpi.py authenticate --url [URL] --username [USERNAME] '
+                       '--password [PASSWORD]')
+    qpi = QPI(app_url=url, token=token)
+    response = qpi.search(query, type, facets, page, rpp, sort)
+    pprint(response.json())
+
+
+@cli.command()
+@click.option('--username',
+              prompt=True,
+              default=lambda: os.environ.get('QUICKPIN_USER', ''))
+@click.option('--password',
+              prompt=True,
+              hide_input=True,
+              default=lambda: os.environ.get('QUICKPIN_PASSWORD', ''))
+@click.option('--url',
+              prompt=True,
+              default=lambda: os.environ.get('QUICKPIN_URL', ''))
+def authenticate(url, username, password):
+    qpi = QPI(app_url=url, username=username, password=password)
+    if qpi.authenticated:
+        click.echo('Token obtained, now set `QUICKPIN_TOKEN` environment '
+                   'variable as "{}"'.format(qpi.token))
+        click.echo('e.g. export QUICKPIN_TOKEN="{}"'.format(qpi.token))
+    else:
+        click.echo('Something went wrong :-(')
+
 
 if __name__ == '__main__':
     cli()
